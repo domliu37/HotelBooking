ba27c3a6d677de7dc34dda829bbe2cc694433c55
domliu37 <domliu37@hotmail.com>
2020-03-02 00:36:21 +1300

    General features/ planning the project
    Navcontroller/tabs/services/main routing configration/lazy routing

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Stamford Plaza Auckland',
+      'High-end hotel with plush quarters, a fitness centre & an indoor pool, plus Thai & Japanese dining.',
+      'assets/Stamford.jpg',
+      258
+    ),
+    new Place(
+      'p2',
+      'Hilton Auckland',
+      'Contemporary waterfront hotel offering a seafood restaurant & an outdoor heated lap pool.',
+      'assets/Hilton.jpg',
+      428
+    ),
+    new Place(
+      'p3',
+      'AHeritage Auckland',
+      'Refined hotel with a restaurant & a chic bar, as well as a rooftop pool & a tennis court.',
+      'assets/Heritage.jpg',
+      223
+    )
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+
+  constructor() { }
+}
