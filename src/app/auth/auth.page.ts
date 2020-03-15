f3dc213e9a314b9c26d9e13aaa0beeed50ea3295
domliu37 <domliu37@hotmail.com>
2020-03-15 23:08:13 +1300

    Auth page UI set up

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
+import { r3JitTypeSourceSpan } from '@angular/compiler';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +12,7 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -17,6 +20,26 @@ export class AuthPage implements OnInit {
     private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // send a request to login servers
+    } else {
+      // send a request to signup servers
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+
   }
 
   onLogin() {
